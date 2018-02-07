2076beab2e4c7e9620a4bb05f43028e3c861a8e8
Sivuyile Magutywa <s.magutywa@applicafro.co.za>
2018-02-07 08:51:44 +0200

    styled app

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+
+import { Flashlight } from '@ionic-native/flashlight';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  by = 'By Applicafro';
+  
 
-  constructor(public navCtrl: NavController) {
-
+  flashOn = false;
+  c = 1;
+  constructor(public navCtrl: NavController, private flashlight: Flashlight, private platform: Platform) {
+  //  flashlight.available().then(av =>{
+  //    if(av) {
+  //      this.checkFlash();
+  //    }
+  //  })
+    
   }
 
+
+  turnOnOff() {
+      this.flashlight.toggle();
+      this.flashOn = !this.flashOn;
+  }
+
+ 
 }
